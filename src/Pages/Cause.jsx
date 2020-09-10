40cb4152a8db1b72e77debc1004631a035546edb
Alexander <rjii0001@student.monash.edu>
2020-09-10 11:27:19 +0800

    add one picture

--- src/Pages/Cause.jsx
+++ src/Pages/Cause.jsx
@@ -22,7 +22,7 @@ import soilTemp from "../asset/pic_userstory_2/PictureI1.png";
 import soilMoisture from "../asset/pic_userstory_2/pictureI2.png";
 import timeDifferent from "../asset/pic_userstory_2/PictureI3.png";
 import saveWater from "../asset/pic_userstory_2/pictureI4.png";
-import test from "../asset/waterDrop.jpg";
+import test from "../asset/desert2.png";
 
 import TagTool from "../components/TagTool";
 
