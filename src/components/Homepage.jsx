384f3dcf9bb0cc9b570449d3144704175026e013
DD <ydai35@student.moansh.edu>
2020-09-26 12:02:41 +0800

    Homepage style change.
    Use a fashion swiper to deco our homepage.
    A vertical slide with mouse control for basic pages.
    A cover-flow effect for cards within each pages.
    Each page is suitable for 1366*1024.
    1280*960 is bit wired.

---
+++ src/components/Homepage.jsx
@@ -0,0 +1,111 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { Typography } from "@material-ui/core";
+import { flash } from "react-animations";
+import Radium, { StyleRoot } from "radium";
+
+//asset
+import main_background from "../asset/homePage/main_background.jpg";
+import DownArrow from "../asset/homePage/downArrow.png";
+
+const useStyles = makeStyles((theme) => ({
+  title: {
+    position: "relative",
+    paddingTop: "350px",
+  },
+  eachImgSlide: {
+    width: "100%",
+    height: "100%",
+  },
+}));
+
+const styles = {
+  flash: {
+    animation: "flash 2s infinite",
+    animationName: Radium.keyframes(flash, "flash"),
+  },
+};
+
+const imgSlideStyle = {
+  backgroundPosition: "center",
+  backgroundRepeat: "no-repeat",
+  width: "auto",
+  height: "1010px",
+};
+
+const scrollToRef = (ref) => window.scrollTo(0, ref.current.offsetTop);
+
+const Homepage = (props) => {
+  const classes = useStyles();
+
+  return (
+    <>
+      <div
+        style={{
+          ...imgSlideStyle,
+          backgroundSize: "cover",
+          backgroundImage: `url(${main_background})`,
+        }}
+      >
+        <div className={classes.title}>
+          <Typography
+            component="h1"
+            variant="h2"
+            align="center"
+            color="textPrimary"
+            gutterBottom
+          >
+            Learn Everything About Drought
+          </Typography>
+          <Typography
+            variant="h5"
+            align="center"
+            color="textSecondary"
+            paragraph
+          >
+            Learn and test your understanding of the drought in Australia in
+            class
+          </Typography>
+        </div>
+        <StyleRoot>
+          <div style={styles.flash}>
+            <Typography
+              variant="h5"
+              align="center"
+              color="textSecondary"
+              paragraph
+              style={{
+                position: "relative",
+                marginTop: "350px",
+              }}
+            >
+              Scroll Down
+            </Typography>
+            <Typography
+              variant="h5"
+              align="center"
+              color="textSecondary"
+              paragraph
+            >
+              <img
+                src={DownArrow}
+                alt="..."
+                style={{
+                  position: "relative",
+                  //   marginTop: "350px",
+                  //   left: "48%",
+                  width: "50px",
+                  height: "50px",
+                  color: "black",
+                }}
+                // onClick={() => scrollToRef(props.myRef)}
+              />
+            </Typography>
+          </div>
+        </StyleRoot>
+      </div>
+    </>
+  );
+};
+
+export default Homepage;
